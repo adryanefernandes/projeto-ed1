daba9d490d5d06747015c2bec1ee768d71b15bb0
adryanefernandes <edryfernandes@gmail.com>
2022-12-12 17:08:56 -0300

    Mudanças para melhor legibilidade e funcionamento do código

--- main.c
+++ main.c
@@ -2,110 +2,170 @@
 #include "locale.h"
 
 #define TAM_VET 5
-#define VERD 1
-#define FALSO 0
 #define ERRO -1
+#define VERDADEIRO 1
+#define FALSO 0
 
 struct Aluno
 {
     char nome[50];
-    int telefone[11];
+    int telefone;
     int idade;
 };
 
-void iniciaPilha(int *Topo)
-{
-    *Topo = -1;
-}
+int menu();
+void fila();
 
-int pilhaCheia(int *Topo)
-{
-    if (*Topo == TAM_VET - 1)
-        return (VERD);
-    return (FALSO);
-}
+// Funções usadas na fila
+int menuFila();
+void iniciarFila(int *inicio, int *termino);
+int insereFila(struct Aluno F[], int *termino);
+int removeFila(struct Aluno F[], int *inicio, int *termino, struct Aluno *valor);
+void exibirFila(struct Aluno F[], int *inicio, int *termino);
 
-int pilhaVazia(int *Topo)
-{
-    if (*Topo == -1)
-        return (VERD);
-    return (FALSO);
-}
+// Funções usadas na pilha
+void pilha();
+int menuPilha();
+void iniciaPilha(int *topo);
+int push(struct Aluno P[], int *topo);
+int pop(struct Aluno P[], int *topo, struct Aluno *valor);
+int pilhaCheia(int *topo);
+int pilhaVazia(int *topo);
+void exibirVetor(struct Aluno P[], int *topo);
 
-int push(struct Aluno P[], int *Topo, struct Aluno valor)
+int main()
 {
-    if (pilhaCheia(Topo))
-    {
-        printf("Erro - Ins. Pilha cheia");
-        return (ERRO);
-    }
-    else
-    {
-        (*Topo)++;
-        P[*Topo] = valor;
-    }
-    return (VERD);
-}
+    setlocale(LC_ALL, "Portuguese");
 
-int pop(struct Aluno P[], int *Topo, struct Aluno *valor)
-{
-    if (pilhaVazia(Topo))
+    int opc = menu(opc);
+
+    // Lida com a escolha do usuário
+    switch (opc)
     {
-        printf("Erro - Ins. Pilha vazia");
-        return (ERRO);
-    }
-    else
-    {
-        *valor = P[*Topo];
-        (*Topo)--;
+    case 0:
+        printf("Até a próxima! ;)");
+        return 0;
+    case 1:
+        fila();
+        break;
+    case 2:
+        pilha();
+        break;
     }
 
-    return (VERD);
+    return 0;
 }
 
-void exibirVetor(struct Aluno P[], int *Topo)
+// Primeiro menu
+// Usuário escolhe a estrutura de dados que quer usar ou sai do programa
+int menu()
 {
-    int i;
-    printf("\nExibir elementos preenchidos de Vetor\n");
-    for (i = 0; i <= *Topo; i++)
+    int opc;
+    do
     {
-        printf("\nElemento [%d]:", i);
-        printf("\nNome: %s", P[i].nome);
-        printf("\nTelefone: %s", P[i].telefone);
-        printf("\nIdade: %d", P[i].idade);
-    }
-    printf("\n\n");
+        printf("Deseja utilizar qual estrutura?\n");
+        printf("[1] Fila\n");
+        printf("[2] Pilha\n");
+        printf("[0] Sair\n\n");
+        printf("Digite a opção que deseja: ");
+        scanf("%d", &opc);
+    } while (opc != 0 && opc != 1 && opc != 2);
+
+    return opc;
 }
 
-void Inicia_Fila(int *inicio, int *termino)
+// Inicio Fila
+void fila()
 {
-    *inicio = 0;
-    *termino = -1;
+    int inicio, termino, ok;
+    int opc;
+
+    struct Aluno fila[TAM_VET];
+    struct Aluno value;
+    iniciarFila(&inicio, &termino);
+
+    // Lida com a escolha do usuário
+    do
+    {
+        opc = menuFila();
+
+        switch (opc)
+        {
+        case 1:
+            insereFila(fila, &termino);
+            break;
+
+        case 2:
+            ok = removeFila(fila, &inicio, &termino, &value);
+
+            if (ok != ERRO)
+            {
+                printf("\nElemento removido:\n");
+                printf("Nome: %s\n", value.nome);
+                printf("Telefone: %d\n", value.telefone);
+                printf("Idade: %d\n", value.idade);
+            }
+
+            break;
+
+        case 3:
+            exibirFila(fila, &inicio, &termino);
+            break;
+
+        case 4:
+            iniciarFila(&inicio, &termino);
+            break;
+        }
+    } while (opc != 0);
 }
 
-int Fila_Vazia(int inicio, int termino)
+int menuFila()
 {
-    if (termino == TAM_VET - 1)
-        return (VERD);
-    return (FALSO);
+    int opc;
+
+    do
+    {
+        printf("\n\nMenu:\n");
+        printf("[1] Inserir\n");
+        printf("[2] Excluir\n");
+        printf("[3] Listar\n");
+        printf("[4] Reiniciar Fila\n");
+        printf("[0] Fim\n");
+        printf("\nDigite a opção escolhida: ");
+        scanf("%d", &opc);
+    } while (opc != 0 && opc != 1 && opc != 2 && opc != 3 && opc != 4);
+
+    return opc;
 }
 
-int Insere_Fila(struct Aluno F[], int *termino, struct Aluno valor)
+int insereFila(struct Aluno fila[], int *termino)
 {
+    struct Aluno value;
+
+    printf("\nDigite as informações do aluno: \n\n");
+    printf("Nome: ");
+    scanf("%s", &value.nome);
+
+    printf("Telefone: ");
+    scanf("%d", &value.telefone);
+
+    printf("Idade: ");
+    scanf("%d", &value.idade);
+
     if (*termino == TAM_VET - 1)
     {
         printf("Erro - Ins. Pilha cheia");
-        return (ERRO);
+        return (VERDADEIRO);
     }
     else
     {
         (*termino)++;
-        F[*termino] = valor;
+        fila[*termino] = value;
     }
-    return (VERD);
+    return (VERDADEIRO);
 }
 
-int Remove_Fila(struct Aluno F[], int *inicio, int *termino, struct Aluno *valor)
+int removeFila(struct Aluno F[], int *inicio, int *termino, struct Aluno *valor)
 {
     if (*inicio > *termino)
     {
@@ -118,104 +178,160 @@ int Remove_Fila(struct Aluno F[], int *inicio, int *termino, struct Aluno *valor
         (*inicio)++;
     }
 
-    return (VERD);
+    return (VERDADEIRO);
 }
 
-void Exibir_Fila(struct Aluno F[], int *inicio, int *termino)
+void exibirFila(struct Aluno F[], int *inicio, int *termino)
 {
     int i;
-    printf("\nListar Fila:");
+    printf("\nFila:\n");
     for (i = *inicio; i <= *termino; i++)
     {
-        printf("\nElemento [%d]:", i);
+        printf("\n\nElemento [%d]:", i);
         printf("\nNome: %s", F[i].nome);
-        printf("\nTelefone: %s", F[i].telefone);
+        printf("\nTelefone: %d", F[i].telefone);
         printf("\nIdade: %d", F[i].idade);
     }
     printf("\n\n");
 }
 
-int main()
+void iniciarFila(int *inicio, int *termino)
 {
-    setlocale(LC_ALL, "Portuguese");
+    *inicio = 0;
+    *termino = -1;
+}
+// Fim Fila
 
-    struct Aluno F[TAM_VET];
+// Inicio pilha
+void pilha()
+{
+    int topo, opc, ok;
     struct Aluno P[TAM_VET];
-    int inicio, termino, Topo;
-    int ok, op = 1;
-
     struct Aluno value;
 
-    Inicia_Fila(&inicio, &termino);
-    while (op != 5)
+    iniciaPilha(&topo);
+
+    do
     {
-        printf("\n\nMenu\n1-Inserir\n2-Excluir");
-        printf("\n3-Listar\n4-Reiniciar Fila");
-        printf("\n5-Fim\nOpcao: \n\n\n");
-        scanf("%d", &op);
+        opc = menuPilha();
 
-        if (op == 1)
+        switch (opc)
         {
-            printf("\nDigite o nome do aluno: ");
-            scanf("%s", value.nome);
+        case 1:
+            push(P, &topo);
+            break;
+        case 2:
+            ok = pop(P, &topo, &value);
 
-            printf("\nDigite o telefone do aluno: ");
-            scanf("%s", value.telefone);
-
-            printf("\nDigite a idade do aluno: ");
-            scanf("%d", &value.idade);
-
-            Insere_Fila(F, &termino, value);
-        }
-        else if (op == 2)
-        {
-            ok = Remove_Fila(F, &inicio, &termino, &value);
-
-            if (ok != ERRO)
-                printf("\nElemento removido: %f", value);
-        }
-        else if (op == 3)
-        {
-            Exibir_Fila(F, &inicio, &termino);
-        }
-        else if (op == 4)
-        {
-            Inicia_Fila(&inicio, &termino);
-        }
-    }
-
-    iniciaPilha(&Topo);
-    
-    while (op != 0)
-    {
-        printf("\n\nMenu\n1-Inserir\n2-Excluir\n3-Exibir Vetor\n0-Fim\nOpcao: ");
-        scanf("%d", &op);
-
-        if (op == 1)
-        {
-            printf("\nDigite o nome do aluno: ");
-            scanf("%s", value.nome);
-
-            printf("\nDigite o telefone do aluno: ");
-            scanf("%s", value.telefone);
-
-            printf("\nDigite a idade do aluno: ");
-            scanf("%d", &value.idade);
-
-            push(P, &Topo, value);
-        }
-        else if (op == 2)
-        {
-            ok = pop(P, &Topo, &value);
             if (ok != ERRO)
             {
-                printf("\n Elemento removido: %s", value.nome);
+                printf("\nElemento removido:\n");
+                printf("Nome: %s\n", value.nome);
+                printf("Telefone: %d\n", value.telefone);
+                printf("Idade: %d\n", value.idade);
             }
+
+            break;
+        case 3:
+            exibirVetor(P, &topo);
+            break;
         }
-        else if (op == 3)
-        {
-            exibirVetor(P, &Topo);
-        }
+
+    } while (opc != 0);
+}
+
+int menuPilha()
+{
+    int opc;
+
+    do
+    {
+        printf("\n\nMenu:\n");
+        printf("[1] Inserir\n");
+        printf("[2] Excluir\n");
+        printf("[3] Exibir vetor\n");
+        printf("[0] Fim\n");
+        printf("\nDigite a opção escolhida:");
+        scanf("%d", &opc);
+    } while (opc != 0 && opc != 1 && opc != 2 && opc != 3);
+
+    return opc;
+}
+
+void iniciaPilha(int *topo)
+{
+    *topo = -1;
+}
+
+// Insere na pilha
+int push(struct Aluno P[], int *topo)
+{
+    struct Aluno value;
+
+    printf("\nDigite as informações do aluno: \n\n");
+    printf("Nome: ");
+    scanf("%s", &value.nome);
+
+    printf("Telefone: ");
+    scanf("%d", &value.telefone);
+
+    printf("Idade: ");
+    scanf("%d", &value.idade);
+
+    if (pilhaCheia(topo))
+    {
+        printf("Erro - Ins. Pilha cheia");
+        return (ERRO);
     }
-    return 0;
+    else
+    {
+        (*topo)++;
+        P[*topo] = value;
+    }
+    return (VERDADEIRO);
+}
+
+int pilhaCheia(int *topo)
+{
+    if (*topo == TAM_VET - 1)
+        return (VERDADEIRO);
+    return (FALSO);
+}
+
+// Remove item da pilha
+int pop(struct Aluno P[], int *topo, struct Aluno *valor)
+{
+    if (pilhaVazia(topo))
+    {
+        printf("Erro - Ins. Pilha vazia");
+        return (ERRO);
+    }
+    else
+    {
+        *valor = P[*topo];
+        (*topo)--;
+    }
+
+    return (VERDADEIRO);
+}
+
+int pilhaVazia(int *topo)
+{
+    if (*topo == -1)
+        return (VERDADEIRO);
+    return (FALSO);
+}
+
+void exibirVetor(struct Aluno P[], int *topo)
+{
+    int i;
+    printf("\n\nExibir elementos preenchidos de Vetor\n");
+    for (i = 0; i <= *topo; i++)
+    {
+        printf("\nElemento [%d]:", i);
+        printf("\nNome: %s", P[i].nome);
+        printf("\nTelefone: %d", P[i].telefone);
+        printf("\nIdade: %d", P[i].idade);
+    }
+    printf("\n\n");
 }
